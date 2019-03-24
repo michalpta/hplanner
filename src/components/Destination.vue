5a8d140807e1b272bfd37d1350ff8857f3bffc47
cinku <cinq2ez@gmail.com>
2019-03-24 18:06:22 +0100

    add picker

---
+++ src/components/Destination.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <select v-model="city">
+            <option v-for="(city, index) of cities" :key="index">{{ city.name }}</option>
+        </select>
+        <select v-model="month">
+            <option v-for="(date, index) of dates" :key="index">{{ date }}</option>
+        </select>
+        <button @click="submit">Submit</button>
+    </div>
+</template>
+
+<script>
+import months from '../data/months';
+
+export default {
+    props: ['cities'],
+    data() {
+        return {
+            city: '',
+            month: '',
+        };
+    },
+    computed: {
+        dates() {
+            return months;
+        },
+    },
+    methods: {
+        submit() {
+            this.$emit('submitLocation', { city: this.city, month: this.month });
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
