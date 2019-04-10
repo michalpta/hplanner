e1247582647696c1fb65647de0a5dbcfc1ed6f6b
Michal Ptaszek <michal.ptaszek@capgemini.com>
2019-04-10 10:31:09 +0200

    Change circle growing mechanism

--- src/components/Map/MapCircle.vue
+++ src/components/Map/MapCircle.vue
@@ -3,16 +3,16 @@
     <l-marker :lat-lng="center">
       <l-tooltip>
         <b>{{name}}</b><br />
-        <span v-if="hits" style="color: gray;">
+        <span style="color: gray;">
           Liczba osób: {{hits}}
         </span>
       </l-tooltip>
     </l-marker>
     <l-circle :lat-lng="center" :radius="radius" :color="color" :fill-color="color"></l-circle>
     <l-circle
-      v-if="doneRatio > 0"
+      v-if="hitsDone > 0"
       :lat-lng="center"
-      :radius="doneRatio * radius"
+      :radius="(hitsDone / hits) * radius"
       :color="'green'"
       :fill-color="'green'"
     ></l-circle>
@@ -24,7 +24,6 @@ import {
   LCircle, LTooltip, LFeatureGroup, LMarker,
 } from 'vue2-leaflet';
 import { getRequestsCollection } from '../../firebase';
-
 export default {
   components: {
     LCircle,
@@ -37,31 +36,22 @@ export default {
     return {
       color: '#0C81E4',
       hits: 0,
+      hitsDone: 0,
       radius: 0,
       maxRadius: 400000,
-      minRadius: 80000,
-      doneRatio: 0,
-      growthSlowingFactor: 10,
+      minRadius: 20000,
+      radiusIncrement: 10000,
     };
   },
   mounted() {
-    getRequestsCollection().where('canceled', '==', false).onSnapshot(((snapshot) => {
-      const totalHits = snapshot.docs.length;
-      if (totalHits > 0) {
-        const requestsForCity = snapshot.docs.filter(r => r.data().city === this.engName);
-        this.hits = requestsForCity.length;
-        this.hitsDone = requestsForCity.filter(r => r.data().status === 'done').length;
-        this.doneRatio = this.hitsDone / this.hits;
-        const radiusRange = this.maxRadius - this.minRadius;
-        this.radius = (this.hits / totalHits) * radiusRange
-          * (Math.min(this.growthSlowingFactor, totalHits) / this.growthSlowingFactor)
-          + this.minRadius;
-      } else {
-        this.hits = 0;
-        this.hitsDone = 0;
-        this.doneRatio = 0;
-        this.radius = 0;
-      }
+    getRequestsCollection().onSnapshot(((snapshot) => {
+      const requests = snapshot.docs.filter(r => !(r.data().canceled));
+      const totalHits = requests.length;
+      const requestsForCity = requests.filter(r => r.data().city === this.engName);
+      this.hits = requestsForCity.length;
+      const requestsForCityDone = requestsForCity.filter(r => r.data().status === 'done');
+      this.hitsDone = requestsForCityDone.length;
+      this.radius = Math.min(hits * radiusIncrement + this.minRadius, this.maxRadius);
     }));
   },
 };
